a087bae9aa4442bac62a49b1e73512e19e9b51a4
Sudharshan <sudharshan@olivetech.net>
2017-08-23 14:41:05 +0530

    Angular 2.0 Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { LoginComponent } from './login.component'
+import { RegisterComponent } from './register.component'
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+const appRoutes: Routes = [
+  { path: 'Login', component: LoginComponent },
+  { path: 'Register', component: RegisterComponent },
+];
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule ({
+  imports: [ BrowserModule,FormsModule,
+    RouterModule.forRoot(appRoutes)],
+  declarations: [ AppComponent,LoginComponent,RegisterComponent],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
